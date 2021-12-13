468999ec3ce066fffc97ad5190012a44e50a12b8
Joel Thibert <Jthibert9839@conestogac.on.ca>
2021-12-12 23:24:47 -0500

    Current

--- saves.c
+++ saves.c
@@ -1,30 +1,42 @@
 #include "saves.h"
-#include "utils.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-void saveBoard(Board_t* saveThis) {
 
-	printf("What is the name of your board?"); // Ask about keeping current name (if existing)?
-	scanf("%s", saveThis->name);
+void saveBoard(Board_t* saveBoards) {
 
-	FILE* file = fopen(saveThis->name, "w");
+	FILE* file = fopen("BoardSaves.txt", "w");
 	if (file != NULL) {
 
-		fwrite(saveThis, sizeof(Board_t), 1, file);
-		fclose(file);
+		int count = 0;
+
+		while ((saveBoards + count) != NULL) {
+
+			for (int i = 0; i < 96; i++) {
+
+				fprintf(file, "%u ", (saveBoards + count)->boardArr + i);
+
+			}
+
+			fprintf(file, ".%s\n", (saveBoards + count)->name);
+
+			++count;
+
+		}
+
+	} else {
+
+		printf("File could not be saved.");
 
 	}
 
-	printf("Board %s has been saved.", saveThis->name);
+	fclose(file);
+
+	printf("File saved successfully.");
 
 }
 
-Board_t* openSave(int selection) { // need to use selection number for determining file selection
-	
-	Board_t* workingBoard = malloc(sizeof(Board_t));
+Board_t* openSave(int selection, Board_t* list) { // not the final version, just for reference
 
-	FILE* file = fopen(, "r"); // need to access information via selection of sort/search results	
+	FILE* file = fopen("BoardSaves.txt", "r");	
 	if (file != NULL) {
 		
 		fread(workingBoard, sizeof(Board_t), 1, file);
@@ -37,3 +49,36 @@ Board_t* openSave(int selection) { // need to use selection number for determini
 
 }
 
+Board_t* loadSaves() {
+
+	Board_t* output = malloc(sizeof(Board_t) * (numberOfSaves + 1));
+
+
+
+}
+
+Board_t* searchSaves(Board_t* searchThis) {
+
+	int count = 0;
+
+	while (searchThis + count != NULL)
+	{
+
+		++count;
+
+	}
+
+}
+
+Board_t* sortBoards(Board_t* sortThis) {
+
+	int count = 0;
+
+	while (sortThis + count != NULL)
+	{
+
+		++count;
+
+	}
+
+}
--- saves.h
+++ saves.h
@@ -1,8 +1,12 @@
-#ifndef FUNCTIONS_H_INCLUDED
-#define FUNCTIONS_H_INCLUDED
+#include "utils.h"
+
+#ifndef SAVES_H_
+#define SAVES_H_
 /* ^^ these are the include guards */
 
 /* Prototypes for the functions */
+
+void saveBoard(Board_t* saveBoards);
 
 
 #endif
